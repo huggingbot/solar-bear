28830e2ba5a5cfce27e8bc5bd53d2d93b519dbc7
huggingbot <83656073+huggingbot@users.noreply.github.com>
2022-05-14 19:24:48 +0800

    chore: Remove prettier integration with solhint;
    chore: Update pre-commit file;

--- contracts/SolarBear.sol
+++ contracts/SolarBear.sol
@@ -1,12 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import 'hardhat/console.sol';
+import "hardhat/console.sol";
 
-import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract SolarBear is ERC1155 {
     constructor(string memory _uri) ERC1155(_uri) {
-        console.log('Deploying SolarBear');
+        console.log("Deploying SolarBear");
     }
 }
