a4bf5fe7be80083268b584b066908cc3557f7516
huggingbot <83656073+huggingbot@users.noreply.github.com>
2022-05-14 23:44:03 +0800

    feat: Add contract deployment optimizer; feat: Add ERC721A and SBREN contracts; feat: Update SolarBear contract to match requirements; chore: Update deployment script; chore: Update solhint ignore list; test: Pause test for now;

--- contracts/SolarBear.sol
+++ contracts/SolarBear.sol
@@ -1,12 +1,34 @@
 //SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+contract SolarBear is Ownable, ERC1155Pausable {
+    uint public constant SOLAR_BEAR = 0;
+    address public immutable sbrenContract;
+    mapping(uint => bool) public tokenClaims;
 
-contract SolarBear is ERC1155 {
-    constructor(string memory _uri) ERC1155(_uri) {
-        console.log("Deploying SolarBear");
+    constructor(string memory _uri, address _sbrenContract) ERC1155(_uri) {
+        sbrenContract = _sbrenContract;
+    }
+
+    function mint(uint[] memory tokenIds) public {
+         for (uint i = 0; i < tokenIds.length; i++) {
+             require(tokenClaims[tokenIds[i]] == false, "Token has been claimed");
+             require(IERC721(sbrenContract).ownerOf(tokenIds[i]) == msg.sender, "Sender is not a token owner");
+             tokenClaims[tokenIds[i]] = true;
+         }
+        _mint(msg.sender, SOLAR_BEAR, tokenIds.length, "");
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
     }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -10,11 +10,24 @@ async function main() {
   console.log('Deploying contracts with the account:', deployer.address);
   console.log('Account balance:', (await deployer.getBalance()).toString());
 
+  const SBREN = await ethers.getContractFactory('SBREN');
+  const sbren = await SBREN.deploy(
+    'Soulbond - Ren Empire',
+    'SBREN',
+    'ipfs://QmNXTanDbZqf1xP5a3RnzcNEeBtSatoCP31Mc8UhRxcbz9/',
+    'ipfs://QmNV4rMvay1kjCoiZ8kbbXLwJYANGHfZjMFznwJZ1U3rGa/hidden_metadata.json',
+    '0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B',
+    '0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B',
+    '0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B'
+  );
+
+  await sbren.deployed();
+  console.log('SBREN deployed to:', sbren.address);
+
   const SolarBear = await ethers.getContractFactory('SolarBear');
-  const solarBear = await SolarBear.deploy('https://token-cdn-domain/{id}.json');
+  const solarBear = await SolarBear.deploy('https://token-cdn-domain/{id}.json', sbren.address);
 
   await solarBear.deployed();
-
   console.log('SolarBear deployed to:', solarBear.address);
 }
 
