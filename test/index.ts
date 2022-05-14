da637894b1bcaf418ceb75f1ad23db0af3ed6cb9
huggingbot <83656073+huggingbot@users.noreply.github.com>
2022-05-14 17:00:28 +0800

    initial commit

---
+++ test/index.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe('SolarBear', function () {
+  it('Should have the correct uri passed in from the constructor', async function () {
+    const SolarBear = await ethers.getContractFactory('SolarBear');
+    const solarBear = await SolarBear.deploy('https://token-cdn-domain/{id}.json');
+    await solarBear.deployed();
+
+    expect(await solarBear.uri('0')).to.equal('https://token-cdn-domain/{id}.json');
+  });
+});
