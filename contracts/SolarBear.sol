c1fed507ee7af145f3464449faaa671a5413908f
huggingrobot <huggingrobot@gmail.com>
2022-05-15 21:31:40 +0800

    -Added getTokenIds function to retrieve list of sbren token ids -Added getFilteredTokenIds to get minted/unminted list of token ids

--- contracts/SolarBear.sol
+++ contracts/SolarBear.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 
 contract SolarBear is Ownable, ERC1155Pausable {
@@ -30,5 +31,33 @@ contract SolarBear is Ownable, ERC1155Pausable {
 
     function unpause() public onlyOwner {
         _unpause();
+    }
+
+    function getTokenIds() public view returns (uint[] memory){
+        uint256 balance = IERC721Enumerable(sbrenContract).balanceOf(msg.sender);
+        uint[] memory tokenIds = new uint[](balance);
+        for (uint i = 0; i < balance; i++) {
+            tokenIds[i] = IERC721Enumerable(sbrenContract).tokenOfOwnerByIndex(msg.sender, i);
+        }
+        return tokenIds;
+    }
+
+    function getFilteredTokenIds(bool minted) public view returns (uint[] memory){
+        uint[] memory tokenIds = getTokenIds();
+        uint counter = 0;
+        for (uint i = 0; i < tokenIds.length; i++) {
+            if(tokenClaims[tokenIds[i]] == true){                
+                counter++;
+            }
+        }        
+        uint[] memory filteredTokenIds = new uint[](counter);
+        counter = 0;
+        for (uint i = 0; i < tokenIds.length; i++) {
+            if(tokenClaims[tokenIds[i]] == minted){
+                filteredTokenIds[counter] = tokenIds[i];
+                counter++;
+            }
+        }     
+        return filteredTokenIds;
     }
 }
