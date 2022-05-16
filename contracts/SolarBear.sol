68114180de9de5dac64bb8491384d8b0ad57c7a2
huggingbot <83656073+huggingbot@users.noreply.github.com>
2022-05-16 20:56:22 +0800

    fix: Pause will stop transfer as well as mint; feat: Update to AccessControl for more flexibility; feat: Add setURI to update the URI just in case;

--- contracts/SolarBear.sol
+++ contracts/SolarBear.sol
@@ -1,36 +1,49 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 
-contract SolarBear is Ownable, ERC1155Pausable {
+contract SolarBear is ERC1155, AccessControl, Pausable {
     uint public constant SOLAR_BEAR = 0;
+
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
     address public immutable sbrenContract;
     mapping(uint => bool) public tokenClaims;
 
     constructor(string memory _uri, address _sbrenContract) ERC1155(_uri) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(OPERATOR_ROLE, msg.sender);
         sbrenContract = _sbrenContract;
     }
 
-    function mint(uint[] memory tokenIds) public {
-         for (uint i = 0; i < tokenIds.length; i++) {
-             require(tokenClaims[tokenIds[i]] == false, "Token has been claimed");
-             require(IERC721(sbrenContract).ownerOf(tokenIds[i]) == msg.sender, "Sender is not a token owner");
-             tokenClaims[tokenIds[i]] = true;
-         }
+    function mint(uint[] memory tokenIds) public whenNotPaused {        
+        for (uint i = 0; i < tokenIds.length; i++) {
+            require(tokenClaims[tokenIds[i]] == false, "Token has been claimed");
+            require(IERC721(sbrenContract).ownerOf(tokenIds[i]) == msg.sender, "Sender is not a token owner");
+            tokenClaims[tokenIds[i]] = true;
+        }
         _mint(msg.sender, SOLAR_BEAR, tokenIds.length, "");
     }
 
-    function pause() public onlyOwner {
+    function setURI(string memory newuri) public onlyRole(OPERATOR_ROLE) {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyRole(OPERATOR_ROLE) {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public onlyRole(OPERATOR_ROLE) {
         _unpause();
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
     function getTokenIds() public view returns (uint[] memory){
