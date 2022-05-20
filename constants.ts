5ad9034a7880bb4dddce2a4cd7515243bd2e6c0d
huggingbot <83656073+huggingbot@users.noreply.github.com>
2022-05-20 23:35:38 +0800

    fix: Put gas price as undefined by default

--- constants.ts
+++ constants.ts
@@ -2,4 +2,5 @@ import { ethers } from 'ethers';
 
 export const SBREN_CONTRACT_ADDRESS = '0xaCc2Fcc87F57C52F945E3F373B32264E76DcFF84';
 export const SOLAR_BEAR_TOKEN_URI = 'https://token-cdn-domain/{id}.json';
-export const GAS_PRICE = ethers.utils.parseUnits('40', 'gwei');
+// export const GAS_PRICE = ethers.utils.parseUnits('40', 'gwei');
+export const GAS_PRICE = undefined;
