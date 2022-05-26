4ed61ab5f700207014603c8a1948f6a104331d16
huggingRobot <huggingrobot@gmail.com>
2022-05-26 14:53:15 +0800

    feat: Allow dynamically add warpet feat: Allow multi warpet per erc721a contract feat: Added switchNation function to change active erc721a contract

--- contracts/SoulbondWarPets.sol
+++ contracts/SoulbondWarPets.sol
@@ -12,9 +12,10 @@ contract SoulbondWarPets is ERC1155, AccessControl, Pausable {
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
     
     string public name;
-    address public activeContract;
+    // address public activeContract;
     uint public warPetId;
-    mapping(address => bool[10000]) public tokenClaims;
+    mapping(uint => address) public warPetToNation;
+    mapping(uint => bool[10000]) public tokenClaims;
 
     constructor(
         string memory _name,
@@ -25,15 +26,15 @@ contract SoulbondWarPets is ERC1155, AccessControl, Pausable {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(OPERATOR_ROLE, msg.sender);
         name = _name;
-        activeContract = _activeContract;
         warPetId = _warPetId;
+        warPetToNation[_warPetId] = _activeContract;
     }
 
     function mint(uint[] memory tokenIds) public whenNotPaused {
         for (uint i = 0; i < tokenIds.length; i++) {
-            require(tokenClaims[activeContract][tokenIds[i]] == false, "Token has been claimed");
-            require(IERC721(activeContract).ownerOf(tokenIds[i]) == msg.sender, "Sender is not a token owner");
-            tokenClaims[activeContract][tokenIds[i]] = true;
+            require(tokenClaims[warPetId][tokenIds[i]] == false, "Token has been claimed");
+            require(IERC721(warPetToNation[warPetId]).ownerOf(tokenIds[i]) == msg.sender, "Sender is not a token owner");
+            tokenClaims[warPetId][tokenIds[i]] = true;
         }
         _mint(msg.sender, warPetId, tokenIds.length, "");
     }
@@ -51,8 +52,9 @@ contract SoulbondWarPets is ERC1155, AccessControl, Pausable {
     }
 
     function switchNation(address _activeContract, uint _warPetId) public onlyRole(OPERATOR_ROLE) {
-        activeContract = _activeContract;
+        require(warPetToNation[_warPetId] == address(0), "This war pet belongs to other nation");
         warPetId = _warPetId;
+        warPetToNation[_warPetId] = _activeContract;
     }
 
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, AccessControl) returns (bool) {
@@ -60,10 +62,10 @@ contract SoulbondWarPets is ERC1155, AccessControl, Pausable {
     }
 
     function getTokenIds(address account) public view returns (uint[] memory){
-        uint256 balance = IERC721Enumerable(activeContract).balanceOf(account);
+        uint256 balance = IERC721Enumerable(warPetToNation[warPetId]).balanceOf(account);
         uint[] memory tokenIds = new uint[](balance);
         for (uint i = 0; i < balance; i++) {
-            tokenIds[i] = IERC721Enumerable(activeContract).tokenOfOwnerByIndex(account, i);
+            tokenIds[i] = IERC721Enumerable(warPetToNation[warPetId]).tokenOfOwnerByIndex(account, i);
         }
         return tokenIds;
     }
@@ -72,14 +74,14 @@ contract SoulbondWarPets is ERC1155, AccessControl, Pausable {
         uint[] memory tokenIds = getTokenIds(account);
         uint counter = 0;
         for (uint i = 0; i < tokenIds.length; i++) {
-            if(tokenClaims[activeContract][tokenIds[i]] == minted){                
+            if(tokenClaims[warPetId][tokenIds[i]] == minted){                
                 counter++;
             }
         }        
         uint[] memory filteredTokenIds = new uint[](counter);
         counter = 0;
         for (uint i = 0; i < tokenIds.length; i++) {
-            if(tokenClaims[activeContract][tokenIds[i]] == minted){
+            if(tokenClaims[warPetId][tokenIds[i]] == minted){
                 filteredTokenIds[counter] = tokenIds[i];
                 counter++;
             }
