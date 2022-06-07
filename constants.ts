4f91bb23c0eb7535dcc1d26c2d73682587cda721
huggingRobot <huggingrobot@gmail.com>
2022-06-07 16:54:01 +0800

    feat: Update on new constant

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 // import { ethers } from 'ethers';
 
-export const SOULBOND_WAR_PETS_NAME = 'Soulbond - War Pets';
-export const SOULBOND_WAR_PETS_TOKEN_URI = 'ipfs://QmNsoNr6rBXQMvqR28A8RCbgH6qysVK9W1DoUyixzE9HTw';
+export const SOULBOND_WAR_PETS_NAME = 'Soulbond - War Pets Collection';
+export const SOULBOND_WAR_PETS_TOKEN_URI = 'ipfs://QmXhcFYLSPzCNrkqn7hWnxzJWt9yrJXmAoRDnV6XYHMCy1';
 export const SBREN_CONTRACT_ADDRESS = '0xaCc2Fcc87F57C52F945E3F373B32264E76DcFF84';
 export const WAR_PET_ID = '0';
 
